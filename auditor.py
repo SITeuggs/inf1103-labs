0d88ea946ede03d809021d82f7d8b18bdc933053
SITeuggs <2603208@sit.singaporetech.edu.sg>
2026-09-11 17:16:41 +0800

    Fixed Bug: Error on line 7 when stock is not an integer

--- auditor.py
+++ auditor.py
@@ -4,10 +4,13 @@ user_quit = True
 
 while user_quit:
     stock = input("Enter the stock Quantity: ")
-    if stock != int and stock < 0:
+    if stock != int or int(stock) < 0:
         print("Enter a valid number.")
     else:
         inventory += stock
-    if stock > 500:
-        print("ALERT! More than 500 stock.")
-        break
+        if stock > 500:
+            print("ALERT! More than 500 stock.")
+            break
+    user_quit = input("Do you want to quit? (Yes or No): ")
+    if user_quit == "Yes":
+        user_quit = False
